242f156e13400b0eaa7ef0520ba916421cb46df3
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-07-14 01:19:43 +0530

    Updates

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: "app-login",
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ["./login.page.scss"],
 })
 export class LoginPage implements OnInit {
-  constructor() {}
+  constructor(private loading:LoadingController,private router:Router) {}
 
   ngOnInit() {}
-  onsubmit(){
+  onsubmit(f:NgForm){
+   this.loading.create({keyboardClose:true,message:"Logging in..."}).then((re)=>{
+     re.present();
+     setTimeout(() => {
+       console.log(f.value.email);
+       re.dismiss();
+     }, 2000);
     
+   })
+     
+      this.router.navigate(['/home'])
   }
 }
