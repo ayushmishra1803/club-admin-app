204f288e32a2346662114d7639ddcdde03880c77
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-07-15 00:39:28 +0530

    Updates

--- src/app/auth/forgotpassword/otp/otp.page.ts
+++ src/app/auth/forgotpassword/otp/otp.page.ts
@@ -1,15 +1,17 @@
+import { NgForm } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-otp',
-  templateUrl: './otp.page.html',
-  styleUrls: ['./otp.page.scss'],
+  selector: "app-otp",
+  templateUrl: "./otp.page.html",
+  styleUrls: ["./otp.page.scss"],
 })
 export class OtpPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  onsubmit(f:NgForm){
+    console.log(f.value.otp);
+    
   }
-
 }
