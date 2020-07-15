6f87681a6a0a1ee93659f7050366ac0b583aa878
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-07-15 21:45:51 +0530

    Updates

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -10,6 +10,22 @@ const routes: Routes = [
   {
     path: 'guestlist',
     loadChildren: () => import('./guestlist/guestlist.module').then( m => m.GuestlistPageModule)
+  },
+  {
+    path: 'table',
+    loadChildren: () => import('./table/table.module').then( m => m.TablePageModule)
+  },
+  {
+    path: 'order',
+    loadChildren: () => import('./order/order.module').then( m => m.OrderPageModule)
+  },
+  {
+    path: 'chat',
+    loadChildren: () => import('./chat/chat.module').then( m => m.ChatPageModule)
+  },
+  {
+    path: 'account',
+    loadChildren: () => import('./account/account.module').then( m => m.AccountPageModule)
   }
 ];
 
