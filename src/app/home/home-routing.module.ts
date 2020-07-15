051a03678ca46cd59776fc4578a953d69182a948
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-07-15 21:55:13 +0530

    Updates

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -4,29 +4,39 @@ import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
-    path: '',
+    path: "tabs",
     component: HomePage,
+    children: [
+      {
+        path: "guestlist",
+        loadChildren: () =>
+          import("./guestlist/guestlist.module").then(
+            (m) => m.GuestlistPageModule
+          ),
+      },
+      {
+        path: "table",
+        loadChildren: () =>
+          import("./table/table.module").then((m) => m.TablePageModule),
+      },
+      {
+        path: "order",
+        loadChildren: () =>
+          import("./order/order.module").then((m) => m.OrderPageModule),
+      },
+      {
+        path: "chat",
+        loadChildren: () =>
+          import("./chat/chat.module").then((m) => m.ChatPageModule),
+      },
+      {
+        path: "account",
+        loadChildren: () =>
+          import("./account/account.module").then((m) => m.AccountPageModule),
+      },
+    ],
   },
-  {
-    path: 'guestlist',
-    loadChildren: () => import('./guestlist/guestlist.module').then( m => m.GuestlistPageModule)
-  },
-  {
-    path: 'table',
-    loadChildren: () => import('./table/table.module').then( m => m.TablePageModule)
-  },
-  {
-    path: 'order',
-    loadChildren: () => import('./order/order.module').then( m => m.OrderPageModule)
-  },
-  {
-    path: 'chat',
-    loadChildren: () => import('./chat/chat.module').then( m => m.ChatPageModule)
-  },
-  {
-    path: 'account',
-    loadChildren: () => import('./account/account.module').then( m => m.AccountPageModule)
-  }
+  { path: "", redirectTo: "/home/tabs/guestlist" },
 ];
 
 @NgModule({
