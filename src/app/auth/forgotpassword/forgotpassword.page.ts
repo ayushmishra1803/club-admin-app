c47a5273164ff654ce497d6ee6ef62cdb9015eaf
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-07-24 03:09:15 +0530

    UPdates

--- src/app/auth/forgotpassword/forgotpassword.page.ts
+++ src/app/auth/forgotpassword/forgotpassword.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
@@ -8,10 +9,18 @@ import { NgForm } from '@angular/forms';
   styleUrls: ["./forgotpassword.page.scss"],
 })
 export class ForgotpasswordPage implements OnInit {
-  constructor(private Router:Router) {}
+  constructor(private Router:Router,private http:HttpClient) {}
 
   ngOnInit() {}
+  email={};
   onsubmit(f:NgForm){
+    this.email = { email: f.value.email };
+    this.http.post(
+      "https://4obg8v558d.execute-api.ap-south-1.amazonaws.com/dev/forgotpasswordotp",this.email
+    ).subscribe(re=>{
+      console.log(re);
+      
+    });
   this.Router.navigate(["forgotpassword/otp"]);
   }
 }
--- src/app/selection/select-club/select-club.page.ts
+++ src/app/selection/select-club/select-club.page.ts
@@ -11,13 +11,15 @@ import { Component, OnInit } from '@angular/core';
 export class SelectClubPage implements OnInit {
 
   constructor(private auth:AuthService,private router:Router,private http:HttpClient) { }
-  city:string
+  city={}
 
   ngOnInit() {
-    this.city=this.auth.getcity();
+    
+    this.city={city:this.auth.getcity()}
     console.log(this.city);
     this.http.get(
-      "https://4obg8v558d.execute-api.ap-south-1.amazonaws.com/dev/clublist"
+      "https://4obg8v558d.execute-api.ap-south-1.amazonaws.com/dev/clublist",this.city
+
     ).subscribe(re=>{
       console.log(re);
       
