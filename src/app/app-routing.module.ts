7f3f5fcac860469872dc6897221bbf165a7de83b
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-07-24 02:38:47 +0530

    uPDATES

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,38 +1,49 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: "home",
+    loadChildren: () =>
+      import("./home/home.module").then((m) => m.HomePageModule),
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full",
   },
   {
-    path: 'login',
-    loadChildren: () => import('./auth/login/login.module').then( m => m.LoginPageModule)
+    path: "login",
+    loadChildren: () =>
+      import("./auth/login/login.module").then((m) => m.LoginPageModule),
   },
   {
-    path: 'forgotpassword',
-    loadChildren: () => import('./auth/forgotpassword/forgotpassword.module').then( m => m.ForgotpasswordPageModule)
+    path: "forgotpassword",
+    loadChildren: () =>
+      import("./auth/forgotpassword/forgotpassword.module").then(
+        (m) => m.ForgotpasswordPageModule
+      ),
   },
   {
-    path: 'select-city',
-    loadChildren: () => import('./selection/select-city/select-city.module').then( m => m.SelectCityPageModule)
+    path: "select-city",
+    loadChildren: () =>
+      import("./selection/select-city/select-city.module").then(
+        (m) => m.SelectCityPageModule
+      ),
   },
   {
-    path: 'select-club',
-    loadChildren: () => import('./selection/select-club/select-club.module').then( m => m.SelectClubPageModule)
+    path: "select-club",
+    loadChildren: () =>
+      import("./selection/select-club/select-club.module").then(
+        (m) => m.SelectClubPageModule
+      ),
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
