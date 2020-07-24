d2ec3535a22d00bc0c1aa2ed0bfd5b546dbe0bda
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-07-25 01:44:14 +0530

    Updates

--- src/app/home/guestlist/guestlist.page.ts
+++ src/app/home/guestlist/guestlist.page.ts
@@ -1,15 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-guestlist',
-  templateUrl: './guestlist.page.html',
-  styleUrls: ['./guestlist.page.scss'],
+  selector: "app-guestlist",
+  templateUrl: "./guestlist.page.html",
+  styleUrls: ["./guestlist.page.scss"],
 })
 export class GuestlistPage implements OnInit {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
-
+  request = [];
+  days: string[] = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
   ngOnInit() {
+    // let day = {
+    //   day: "Friday",
+    // };
+    // this.http
+    //   .post<{ data: any[] }>(
+    //     "https://4obg8v558d.execute-api.ap-south-1.amazonaws.com/dev/guestlist/request",
+    //     day
+    //   )
+    //   .subscribe((re) => {
+    //     this.request = re.data.user_data;
+    //     console.log(this.request);
+    //   });
   }
-
+  selectedday(day:string){
+    console.log(day);
+    
+  }
 }
