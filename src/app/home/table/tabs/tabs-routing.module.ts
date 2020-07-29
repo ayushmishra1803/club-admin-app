c2fe77f7ee833f81e18bffccc257ad643b604f48
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-07-30 00:41:08 +0530

    UPdates

--- src/app/home/table/tabs/tabs-routing.module.ts
+++ src/app/home/table/tabs/tabs-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { TablePage } from './table.page';
+import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: TablePage
-  },
-  {
-    path: 'tabs',
-    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
+    component: TabsPage
   }
 ];
 
@@ -18,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class TablePageRoutingModule {}
+export class TabsPageRoutingModule {}
