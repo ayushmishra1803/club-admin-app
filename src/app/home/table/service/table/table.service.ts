b5cf81f6c2568fcbb69cec31e95dadb7326f0d12
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-07-30 01:49:46 +0530

    Updates

--- src/app/home/table/service/table/table.service.ts
+++ src/app/home/table/service/table/table.service.ts
@@ -8,6 +8,7 @@ export class TableService {
   constructor(private router: Router) {}
   private date: string;
   private day: string;
+  private groupId:string;
   setday_date(day: string, date: string) {
     this.date = date;
     this.day = day;
@@ -19,5 +20,10 @@ export class TableService {
   }
   getday() {
     return this.day;
+  }
+  get_Groupid(id:string){
+    this.groupId=id;
+    console.log(this.groupId);
+    
   }
 }
