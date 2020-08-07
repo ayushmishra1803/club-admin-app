baf00120ffcaf363f1c9077a84e860f1e07153af
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-08-07 22:48:13 +0530

    Updates

--- src/app/home/order/booked-tables/booked-tables.component.ts
+++ src/app/home/order/booked-tables/booked-tables.component.ts
@@ -1,14 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-booked-tables',
-  templateUrl: './booked-tables.component.html',
-  styleUrls: ['./booked-tables.component.scss'],
+  selector: "app-booked-tables",
+  templateUrl: "./booked-tables.component.html",
+  styleUrls: ["./booked-tables.component.scss"],
 })
 export class BookedTablesComponent implements OnInit {
+  constructor(private http: HttpClient, private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  ngOnInit() {
+    let token =
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTk2ODkwNDg5LCJqdGkiOiJkMjhlNTA4YzBmOTI0MWNmODc5ZDJlYjlkZWNhMzJjYSIsInV1aWQiOiIwNTljMThhMi02OWY3LTRlYTAtOTdiZS1kZDEwYTBmYmFiMjEifQ.oqtWfXD91RZm5bqP1Oeq4dNXj7rM9Yseh_ris0-2zkE";
+    let header = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+    let data = {
+      date: "2020-08-10",
+    };
+    this.http.post(
+      "https://4obg8v558d.execute-api.ap-south-1.amazonaws.com/dev/order/bookedtables",
+      data,
+      { headers: header }
+    ).subscribe(re=>{
+      console.log(re);
+      
+    });
+  }
 }
--- src/app/home/order/completed/completed.component.ts
+++ src/app/home/order/completed/completed.component.ts
@@ -1,14 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { order } from './../on-going/interface/onGoing';
+import { Router } from "@angular/router";
+import { HttpClient,HttpHeaders } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-completed',
-  templateUrl: './completed.component.html',
-  styleUrls: ['./completed.component.scss'],
+  selector: "app-completed",
+  templateUrl: "./completed.component.html",
+  styleUrls: ["./completed.component.scss"],
 })
 export class CompletedComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+  constructor(private http: HttpClient, private router: Router) {}
+  completedData:order[]=[]
+  ngOnInit() {
+     let token =
+       "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTk2ODkwNDg5LCJqdGkiOiJkMjhlNTA4YzBmOTI0MWNmODc5ZDJlYjlkZWNhMzJjYSIsInV1aWQiOiIwNTljMThhMi02OWY3LTRlYTAtOTdiZS1kZDEwYTBmYmFiMjEifQ.oqtWfXD91RZm5bqP1Oeq4dNXj7rM9Yseh_ris0-2zkE";
+     let header = new HttpHeaders({
+       Authorization: `Bearer ${token}`,
+     });
+     let data = {
+       date: "2020-08-10",
+       day: "Monday",
+     };
+    this.http
+      .post<{message:string ,data:order[]}>(
+        "https://4obg8v558d.execute-api.ap-south-1.amazonaws.com/dev/order/past",
+        data,
+        { headers: header }
+      )
+      .subscribe((re) => {
+        this.completedData=re.data
+        console.log(re);
+      });
+  }
 }
