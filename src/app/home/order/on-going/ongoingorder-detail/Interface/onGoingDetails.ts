964597d7e9bbb4eef12ed493b40567fbaa99b8f6
khushboothakur2305 <khushboothakur2305@gmail.com>
2020-08-08 00:39:09 +0530

    update

---
+++ src/app/home/order/on-going/ongoingorder-detail/Interface/onGoingDetails.ts
@@ -0,0 +1,9 @@
+export interface onGoingDetails {
+    confirmed_table__date:string,
+    confirmed_table__groups__rating: string,
+    confirmed_table__groups__title:string,
+    confirmed_table__table_booked__table_num:string,
+    items: string[],
+    order_number:string,
+    total_price: string,
+  }
--- src/app/home/order/on-going/ongoingorder-detail/ongoingorder-detail.page.ts
+++ src/app/home/order/on-going/ongoingorder-detail/ongoingorder-detail.page.ts
@@ -2,6 +2,7 @@ import { OrderdetailsService } from "./../../service/order/orderdetails.service"
 import { Router } from "@angular/router";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
+import { onGoingDetails } from './Interface/onGoingDetails';
 
 @Component({
   selector: "app-ongoingorder-detail",
@@ -15,6 +16,7 @@ export class OngoingorderDetailPage implements OnInit {
     private order: OrderdetailsService
   ) {}
   orderId: string;
+  onGoingDetails:onGoingDetails[];
   ngOnInit() {
     let token =
       "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTk2ODkwNDg5LCJqdGkiOiJkMjhlNTA4YzBmOTI0MWNmODc5ZDJlYjlkZWNhMzJjYSIsInV1aWQiOiIwNTljMThhMi02OWY3LTRlYTAtOTdiZS1kZDEwYTBmYmFiMjEifQ.oqtWfXD91RZm5bqP1Oeq4dNXj7rM9Yseh_ris0-2zkE";
@@ -23,11 +25,12 @@ export class OngoingorderDetailPage implements OnInit {
     });
     this.orderId = this.order.getOrderId();
     this.http
-      .get(
+      .get<{ status: boolean, message: string; data: onGoingDetails[] }>(
         `https://4obg8v558d.execute-api.ap-south-1.amazonaws.com/dev/orderdetails/${this.orderId}`,
         { headers: header }
       )
       .subscribe((re) => {
+        this.onGoingDetails=re.data;
         console.log(re);
       });
   }
