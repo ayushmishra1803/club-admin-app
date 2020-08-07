1512b7fb756b32855cd16c7ed309fe2efc4da4e6
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-08-07 22:07:47 +0530

    Updates

---
+++ src/app/home/order/on-going/interface/onGoing.ts
@@ -0,0 +1,7 @@
+export interface order {
+  confirmed_table__groups__title: string;
+  confirmed_table__table_booked__table_num: string;
+  created_at: string;
+  items: string[];
+  uuid:string;
+}
--- src/app/home/order/on-going/on-going.component.ts
+++ src/app/home/order/on-going/on-going.component.ts
@@ -1,14 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { order } from './interface/onGoing';
+import { AuthService } from "./../../../service/auth/auth.service";
+import { Router } from "@angular/router";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-on-going',
-  templateUrl: './on-going.component.html',
-  styleUrls: ['./on-going.component.scss'],
+  selector: "app-on-going",
+  templateUrl: "./on-going.component.html",
+  styleUrls: ["./on-going.component.scss"],
 })
 export class OnGoingComponent implements OnInit {
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private auth: AuthService
+  ) {}
+  onGoingData:order[]=[]
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  ngOnInit() {
+    let token =
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTk2ODkwNDg5LCJqdGkiOiJkMjhlNTA4YzBmOTI0MWNmODc5ZDJlYjlkZWNhMzJjYSIsInV1aWQiOiIwNTljMThhMi02OWY3LTRlYTAtOTdiZS1kZDEwYTBmYmFiMjEifQ.oqtWfXD91RZm5bqP1Oeq4dNXj7rM9Yseh_ris0-2zkE";
+    let header = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+    let data = {
+      date: "2020-08-10",
+      day: "Monday",
+    };
+    this.http
+      .post<{message:string,data:order[]}>(
+        "https://4obg8v558d.execute-api.ap-south-1.amazonaws.com/dev/order/ongoing",
+        data,
+        { headers: header }
+      )
+      .subscribe((re) => {
+        this.onGoingData=re.data
+        console.log(re);
+      });
+  }
 }
