6d249bb46a165765a231821adbbae6c984bd3bd3
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-08-10 01:24:38 +0530

    Updates

--- src/app/home/order/on-going/ongoingorder-detail/order-feedback/order-feedback.component.ts
+++ src/app/home/order/on-going/ongoingorder-detail/order-feedback/order-feedback.component.ts
@@ -1,5 +1,7 @@
-import { ModalController } from '@ionic/angular';
-import { Component, OnInit } from '@angular/core';
+import { OrderdetailsService } from "./../../../service/order/orderdetails.service";
+import { HttpClient,HttpHeaders } from "@angular/common/http";
+import { ModalController } from "@ionic/angular";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-order-feedback",
@@ -7,13 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ["./order-feedback.component.scss"],
 })
 export class OrderFeedbackComponent implements OnInit {
-  constructor(private model: ModalController) {}
-  rating: number = 0;
+  constructor(
+    private model: ModalController,
+    private http: HttpClient,
+    private service: OrderdetailsService
+  ) {}
+  rating: string ;
   ngOnInit() {}
   onsubmit() {
-    console.log(this.rating);
+    let orderId=this.service.getOrderId().trim();
+   console.log(orderId);
+   
+    const data = {
+      rating:parseFloat(this.rating),
+    };
 
-    console.log("Hello");
-    this.model.dismiss({message:"Success"},"success")
+   console.log(data);
+    //  console.log("Hello");
+    //  this.model.dismiss({ message: "Success" }, "success");
+    let token =
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTk3MDc2NTY0LCJqdGkiOiJkZGY0YjlmNjY4ZjU0N2M2Yjk4ZDIxMDc1NGM4Njk2ZCIsInV1aWQiOiIwYTc2MzI2ZC1jZWU4LTRjMzAtYmUyYy03NTgzZDE3ZTg5OGQifQ.HkR_V6xa7jptwSqvXmqVn70vv3IgQ2qHyDPyp5v18a8";
+    let header = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+   
+   this.http.put(
+     `https://4obg8v558d.execute-api.ap-south-1.amazonaws.com/dev/addrating/${orderId}`,data,{headers:header}
+   ).subscribe(re=>{
+     console.log(re);
+     console.log(this.rating);
+
+     console.log("Hello");
+     this.model.dismiss({ message: "Success" }, "success");
+   });
+    
   }
 }
